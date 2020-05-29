d31a5a5060f2aa5cc6a6bf0585e25bc6b6f3625e
Alex Scarborough <alex@teak.io>
2020-05-29 13:49:31 -0700

    No indentation in the epilog.

--- conf.py
+++ conf.py
@@ -58,10 +58,10 @@ source_suffix = '.rst'
 
 # Templating magic for the toctree
 rst_epilog = """
-  .. |air_home| replace:: https://teak.readthedocs.io/projects/air/en/latest
-  .. |unity_home| replace:: https://teak.readthedocs.io/projects/unity/en/latest
-  .. |server_api_home| replace:: https://teak.readthedocs.io/projects/server-api/en/latest
-  .. |general_home| replace:: https://teak.readthedocs.io/en/latest
+.. |air_home| replace:: https://teak.readthedocs.io/projects/air/en/latest
+.. |unity_home| replace:: https://teak.readthedocs.io/projects/unity/en/latest
+.. |server_api_home| replace:: https://teak.readthedocs.io/projects/server-api/en/latest
+.. |general_home| replace:: https://teak.readthedocs.io/en/latest
 """
 
 # The master toctree document.
