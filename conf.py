f98a6d59ec7bdf0385838c2509dc99fe113a5668
Alex Scarborough <alex@teak.io>
2020-05-29 13:50:52 -0700

    Try making it a prolog.

--- conf.py
+++ conf.py
@@ -57,7 +57,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # Templating magic for the toctree
-rst_epilog = """
+rst_prolog = """
 .. |air_home| replace:: https://teak.readthedocs.io/projects/air/en/latest
 .. |unity_home| replace:: https://teak.readthedocs.io/projects/unity/en/latest
 .. |server_api_home| replace:: https://teak.readthedocs.io/projects/server-api/en/latest
